89f5bea030173ba8b46ebab411929f65fd679a6d
Sumit Prasad <sprasa15@partner.jaguarlandrover.com>
2023-07-04 22:55:52 +0100

    add listeners on deviceready event + other changes

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,29 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+const app = {
+  event: null,
+  init: () => {
+    const msg = `${app.event} event was fired`;
+    window.alert(msg);
+    console.log(alert);
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+    app.addListeners();
+  },
+  addListeners: () => {
+    document.querySelector('button#increment').addEventListener('click', app.incrementCount);
+    document.querySelector('button#decrement').addEventListener('click', app.decrementCount);
+  },
+  incrementCount: () => {
+    const h3 = document.querySelector('h3#count');
+    let num = +h3.textContent.split(':')[1].trim();
+    num += 1;
+    h3.textContent = 'The count is: ' + num;
+  },
+  decrementCount: () => {
+    const h3 = document.querySelector('h3#count');
+    let num = +h3.textContent.split(':')[1].trim();
+    num -= 1;
+    h3.textContent = 'The count is: ' + num;
+  }
+};
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+app.event = 'cordova' in window ? 'deviceready' : 'DOMContentLoaded';
+document.addEventListener(app.event, app.init);
